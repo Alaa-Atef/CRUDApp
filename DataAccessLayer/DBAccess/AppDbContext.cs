23cda64ddb9d62a76b71d74ce204f55a283d0be0
Alaa Atef <alaaatef97@gmail.com>
2025-07-09 00:01:35 +0300

    Initial Database Setup with Models
    - Removed Default Weather Controller
    - Removed Default extra Classes
    - Added 2 Models "Student" and "Product"
    - Added a DBContext and added setup the database using entity framework

---
+++ DataAccessLayer/DBAccess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DBAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
