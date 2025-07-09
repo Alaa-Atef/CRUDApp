3c7fe283d0020b9ba0e8805ffca5c7acbd4b2c7e
Alaa Atef <alaaatef97@gmail.com>
2025-07-09 13:18:54 +0300

    Added Validation for Student and Product Models Properties

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -1,9 +1,16 @@
-﻿namespace Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name can't exceed 100 characters.")]
         public string Name { get; set; }
+
+        [Range(0.01, 10000, ErrorMessage = "Price must be at least 0.01.")]
         public float Price { get; set; }
     }
 }
--- Domain/Student.cs
+++ Domain/Student.cs
@@ -1,9 +1,15 @@
-﻿namespace Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
 {
     public class Student
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Student name is required.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Student Name can't exceed 100 characters.")]
         public string Name { get; set; }
+        [Range(5, 120)]
         public int Age { get; set; }
     }
 }
